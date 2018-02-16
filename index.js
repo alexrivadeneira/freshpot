04df6e33fea7cdc8c51adc49b217c3e858a9cb02
alexrivadeneira <alexrivs@gmail.com>
2018-02-16 11:42:32 -0800

    callback and filtering working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sqlite = require('sqlite'),
       sequelize = new Sequelize({dialect: 'sqlite', storage: './db/database.db'}),
       request = require('request'),
       express = require('express'),
+      async = require('async'),
       app = express();
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
@@ -55,67 +56,65 @@ function getFilmRecommendations(req, res) {
 			let filteredRecs = [];
 
 			// for loop: for each matching film, query for its reviews from the 3rd party api 
-			for(let i = 0; i < recs.length; i++){
-				let url = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
-				url += recs[i].id;
 
-				// make the request for each film's reviews
+			// first is the array we're iterating over
+
+// ----> START
+	async.each(recs,
+			function(item, callback){
+				let url = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
+				url += item.id;	
 				request({
 					url: url,
-					json: true
-				}, function(error, response, body){
-					if(error){
-						console.log(error);
-					} else {
-						// only allow through recs with more than 
-						let moreThanFourReviews = false;
-						let reviewAvgRating = 0;
+					json: true,
+					}, 
+						function(error, response, body){
+							if(error){
+								console.log(error);
+							} else {
+								let moreThanFourReviews = false;
+								let reviewAvgRating = 0;
 
-						let reviews = body[0]["reviews"];
-						
-						if(reviews.length > 4){
-							moreThanFourReviews = true;
-							// don't need to get avg rating unless more than 4 reviews, so do that here:
-							for(let j = 0; j < reviews.length; j++){
-								reviewAvgRating += reviews[j].rating;
-							}	
-							reviewAvgRating = reviewAvgRating / reviews.length;
+								let reviews = body[0]["reviews"];
+								if(reviews.length > 4){
+									moreThanFourReviews = true;
+									// don't need to get avg rating unless more than 4 reviews, so do that here:
+									for(let j = 0; j < reviews.length; j++){
+										reviewAvgRating += reviews[j].rating;
+									}	
+									reviewAvgRating = reviewAvgRating / reviews.length;
+								}
+								if(moreThanFourReviews && reviewAvgRating > 4){
+									filteredRecs.push(item);
+								}																	
+							}
+							callback();
 						}
+				);			
+			}, function(err){
+				if(err){
+					console.log(error);
+				} else {
+					res.status(500).send({"recommendations": filteredRecs});
+				}
+			}	
+		);
+// ----> END
 
-						if(moreThanFourReviews && reviewAvgRating > 4){
-							console.log("GOT HERE");
-							console.log(filteredRecs);
-							filteredRecs.push(recs[i]);
-							console.log(i, recs.length);
-						}
 
-						res.status(500).send({"recommendations": filteredRecs});
 
-					}
+				}
+			);
 
-				}); // end of request callback
 
-			} // end of for loop iterating through all of the genre/year matches
 			
 
-		// res.status(500).send({"trash": 3});
 
 		});
-	});
 
-// function sendFilters(items){
-// 	res.status(500).send({"recommendations": items});
-// }
-
-}
+};
 
 
-
-
-function testingCallback(item){
-	console.log("testingCallbakc");
-	console.log("ITEM: ",item);
-}
 function getFilmRecommendationsLimit(req, res){
 	console.log("REQ >>>>", req.params);	
 	res.status(500).send("with limit");
