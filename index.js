c27f61db6edbf1b285e9ff0a191f4a4ffee7c7d0
alexrivadeneira <alexrivs@gmail.com>
2018-02-17 07:18:10 -0800

    reconfig for tests

--- index.js
+++ index.js
@@ -36,105 +36,104 @@ app.get('*', function(req, res) {
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-	return {"recommendations": ["himom"]};
 	let id = req.params.id;
 
-	// // get the initial input film data
-	// Film.findById(id).then(function(myFilm){
-	// 	if(myFilm){
-	// 		console.log(myFilm["dataValues"]);
-	// 		return myFilm["dataValues"];
-	// 	} else {
-	// 		throw new Error("not found");
-	// 		// res.status(404).send("not found");
-	// 	}
-	// // use the original film's data to get all films by same genre and time
-	// }).then(function(film){
-	// 	if(film){
-	// 		// use that film to get proper data
-	// 		// isolate year and rest of the date
-	// 		let year = film.release_date.substring(0, 4);
-	// 		let restDate = film.release_date.substring(4, film.release_date.length);
-	// 		let rangeMax = parseInt(year) + 15;
-	// 		let rangeMin = parseInt(year) - 15;
-	// 		let rangeMinDate = rangeMin + restDate;
-	// 		let rangeMaxDate = rangeMax + restDate;					
+	// get the initial input film data
+	Film.findById(id).then(function(myFilm){
+		if(myFilm){
+			console.log(myFilm["dataValues"]);
+			return myFilm["dataValues"];
+		} else {
+			throw new Error("not found");
+			// res.status(404).send("not found");
+		}
+	// use the original film's data to get all films by same genre and time
+	}).then(function(film){
+		if(film){
+			// use that film to get proper data
+			// isolate year and rest of the date
+			let year = film.release_date.substring(0, 4);
+			let restDate = film.release_date.substring(4, film.release_date.length);
+			let rangeMax = parseInt(year) + 15;
+			let rangeMin = parseInt(year) - 15;
+			let rangeMinDate = rangeMin + restDate;
+			let rangeMaxDate = rangeMax + restDate;					
 
-	// 		console.log(rangeMinDate);
-	// 		console.log(rangeMaxDate);
+			console.log(rangeMinDate);
+			console.log(rangeMaxDate);
 
-	// 		// let startDate = new Date("October 13, 2000 11:13:00")
-	// 		// let endDate = new Date("October 13, 2001 11:13:00")
+			// let startDate = new Date("October 13, 2000 11:13:00")
+			// let endDate = new Date("October 13, 2001 11:13:00")
 
-	// 		let startDate = new Date(rangeMinDate + ' 00:00:00');
-	// 		let endDate = new Date(rangeMaxDate + ' 12:59:59');
+			let startDate = new Date(rangeMinDate + ' 00:00:00');
+			let endDate = new Date(rangeMaxDate + ' 12:59:59');
 
-	// 		return Film.findAll({
-	// 			where: {
-	// 				genre_id: film.genre_id,
-	// 				release_date:{
-	// 					$between: [startDate, endDate]
-	// 				}
-	// 			}
-	// 		});
+			return Film.findAll({
+				where: {
+					genre_id: film.genre_id,
+					release_date:{
+						$between: [startDate, endDate]
+					}
+				}
+			});
 
-	// 	} else {
-	// 		res.status(404).send("not found");
-	// 	}
-	// }).then(function(films){
-	// 	if(films){
-	// 	let filteredRecs = [];
+		} else {
+			res.status(404).send("not found");
+		}
+	}).then(function(films){
+		if(films){
+		let filteredRecs = [];
 
-	// 		async.each(films,
-	// 			function(film, callback){
-
-
-	// 				let url = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
-	// 				url += film["dataValues"].id;		
-
-	// 				request({
-	// 					url:  url,
-	// 					json: true
-	// 				}, function(error, response, body){
-	// 						if(error){
-	// 							console.log(error);
-	// 						} else {
-	// 							let moreThanFourReviews = false;
-	// 							let reviewAvgRating = 0;
-
-	// 							let reviews = body[0]["reviews"];
-	// 							if(reviews.length > 4){
-	// 								moreThanFourReviews = true;
-	// 								// don't need to get avg rating unless more than 4 reviews, so do that here:
-	// 								for(let j = 0; j < reviews.length; j++){
-	// 									reviewAvgRating += reviews[j].rating;
-	// 								}	
-	// 								reviewAvgRating = reviewAvgRating / reviews.length;
-	// 							}
-	// 							// console.log(moreThanFourReviews, reviewAvgRating);
-	// 							if(moreThanFourReviews && reviewAvgRating > 4){
-	// 								console.log("GOT ONE")
-	// 								filteredRecs.push(film["dataValues"]);
-	// 							}																	
-	// 						}
-	// 						callback();
-	// 					}
-	// 				);
+			async.each(films,
+				function(film, callback){
 
 
-	// 			}, function(error){
-	// 				if(error){ 
-	// 					console.log(error)
-	// 				} else {
-	// 					console.log("GOT HRE");
-	// 					res.json({"recommendations": filteredRecs});
-	// 				}
-	// 			}
-	// 			);
-	// 	} else {
-	// 		console.log("error");
-	// 	}
-	// });
+					let url = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
+					url += film["dataValues"].id;		
+
+					request({
+						url:  url,
+						json: true
+					}, function(error, response, body){
+							if(error){
+								console.log(error);
+							} else {
+								let moreThanFourReviews = false;
+								let reviewAvgRating = 0;
+
+								let reviews = body[0]["reviews"];
+								if(reviews.length > 4){
+									moreThanFourReviews = true;
+									// don't need to get avg rating unless more than 4 reviews, so do that here:
+									for(let j = 0; j < reviews.length; j++){
+										reviewAvgRating += reviews[j].rating;
+									}	
+									reviewAvgRating = reviewAvgRating / reviews.length;
+								}
+								// console.log(moreThanFourReviews, reviewAvgRating);
+								if(moreThanFourReviews && reviewAvgRating > 4){
+									console.log("GOT ONE")
+									filteredRecs.push(film["dataValues"]);
+								}																	
+							}
+							callback();
+						}
+					);
+
+
+				}, function(error){
+					if(error){ 
+						console.log(error)
+					} else {
+						console.log("GOT HRE");
+						res.json({"recommendations": filteredRecs});
+					}
+				}
+				);
+		} else {
+			console.log("error");
+		}
+	});
 
 
 
